3eae8d1842182167eab948b36a7e7a5465554f8d
nsl <156998288+nsllab@users.noreply.github.com>
2024-11-25 19:13:58 +0800

    Removed ClearableFileInput

--- research/forms.py
+++ research/forms.py
@@ -1,12 +1,13 @@
 from .models import History
 from django import forms
 from .models import UserInput
+from django.forms.widgets import MultipleFileInput
 
 class UserInputForm(forms.Form):
     title = forms.CharField(max_length=255)
     text_content = forms.CharField(widget=forms.Textarea, required=False)
     files = forms.FileField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}),
+        widget=MultipleFileInput,  # Use MultipleFileInput for handling multiple files
         required=False,
     )
 
