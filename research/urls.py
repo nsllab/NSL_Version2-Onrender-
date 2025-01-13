2e56ebc1262f01682d0bdb73432f50c94a2762e5
poreocook <78528119+poreocook@users.noreply.github.com>
2025-01-13 09:48:54 +0900

    fix4
    fix4

--- research/urls.py
+++ research/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('purechain/', views.purechain_view, name='purechain_view'),
     path('paperacknowledgement/', views.acknowl_view, name='acknowl_view'),
     path('purechain/delete/<str:filename>/', views.delete_file, name='delete_file'),
+    path('purechain/delete/<str:filename>/', views.ackdelete_file, name='ackdelete_file'),
     path('details/<str:title>/', views.entry_details, name='entry_details'),
+    path('details/<str:title>/', views.ackentry_details, name='ackentry_details'),
 ]
