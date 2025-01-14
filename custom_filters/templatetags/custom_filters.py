0901e6dc5b78af08a45c0bd295152840e6d9da92
poreocook <78528119+poreocook@users.noreply.github.com>
2025-01-14 16:48:41 +0900

    custom filters

--- custom_filters/templatetags/custom_filters.py
+++ custom_filters/templatetags/custom_filters.py
@@ -6,3 +6,15 @@ register = template.Library()
 @register.filter
 def split_string(value, delimiter=','):
     return value.split(delimiter)
+
+@register.filter
+def last_name_first(user):
+    if user.first_name and user.last_name:
+        return f"{user.last_name}, {user.first_name}"
+    return user.username
+
+@register.filter
+def get_initial(user):
+    if user.last_name:
+        return user.last_name[0].upper()
+    return user.username[0].upper()
